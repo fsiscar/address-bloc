2d73ab97578f8a642824690272d6f0afbc9ca44c
fsiscar <fsiscar@gmail.com>
2018-03-26 10:08:11 -0500

    Checkpoint 5 - Menu: completed

--- address_bloc.rb
+++ address_bloc.rb
@@ -1,1 +1,7 @@
-puts "Welcome to AddressBloc"
+require_relative 'controllers/menu_controller'
+
+menu = MenuController.new
+system "clear"
+puts "Welcome to AddressBloc!"
+puts "======================="
+menu.main_menu
